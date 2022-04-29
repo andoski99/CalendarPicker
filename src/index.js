8ca702a96cb038f070f3e39a6b2b191ee8d0673c
andoski99 <goodsportdevelopment@icloud.com>
2022-04-30 05:40:45 +1000

    Updated

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 
-$("#datepicker").daterangepicker();
+
+const pickerOptions = 
+{showISOWeekNumbers: true,
+autoApply: false,};
+
+const loadPicker = (json) => {
+}
+$("#datepicker").daterangepicker(pickerOptions);
+
+// Getting the data back into filemaker
+
+
+
